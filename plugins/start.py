3cc79106571a60806aff32c1e688224d474153e4
Ayush Sharma <aayush240204@gmail.com>
2020-12-10 15:38:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,9 @@ async def start(client, message):
         [InlineKeyboardButton("Channel ❣️", url="https://t.me/AyushBots")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/CyberBoyAyushBot")]
+        [InlineKeyboardButton(
+            "DEVELOPER💻", url="https://github.com/CyberBoyAyush")]
     ])
-    welcomed = f"Hoi! <b>{message.from_user.first_name}</b>\n\nThis Is Youtube Video Downloader Bot🔥\n\nJoin @AyushBots❤️\n\nHit /Help To Know More😁"
+    welcomed = f"Hoi! <b>{message.from_user.first_name}</b>\n\nThis Is Youtube Video Downloader Bot🔥\n\nJoin @AyushBots Before Using It❤️\n\nHit /Help To Know More😁"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
